c820ac1af8ff335977b9a5935c1414df78bae51c
Peter Kofler <peter.kofler@code-cop.org>
2017-03-01 21:14:32 +0100

    Finish #2.

--- nose.bat
+++ nose.bat
@@ -1,1 +1,1 @@
-@FOR /F "usebackq delims==" %%i IN (`dir /s /b test_1_*.py`) DO @call python -W ignore::DeprecationWarning -m nose --stop -q -w %%~pi
+@FOR /F "usebackq delims==" %%i IN (`dir /s /b test_2_*.py`) DO @call python -W ignore::DeprecationWarning -m nose --stop -q -w %%~pi
