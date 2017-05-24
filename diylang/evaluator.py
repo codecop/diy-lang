f5e0f4f13ae16c05193455a3f57c0ec3a3838cf5
Peter Kofler <peter.kofler@code-cop.org>
2017-05-25 00:24:59 +0200

    Finish #4.

--- diylang/evaluator.py
+++ diylang/evaluator.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .types import Environment, DiyLangError, Closure, String
+from .types import DiyLangError, Closure, String
 from .ast import is_boolean, is_atom, is_symbol, is_list, is_closure, \
     is_integer, is_string
 from .parser import unparse
@@ -20,6 +20,9 @@ def evaluate(ast, env):
 
     if is_boolean(ast) or is_integer(ast) or is_string(ast):
         return ast
+
+    if is_symbol(ast):
+        return env.lookup(ast)
 
     if len(ast) == 2 and ast[0] == 'quote':
         return ast[1]
@@ -47,6 +50,18 @@ def evaluate(ast, env):
             return evaluate(ast[2], env)
         else:
             return evaluate(ast[3], env)
+
+    if ast[0] == 'define':
+        if len(ast) != 3:
+            raise DiyLangError("Wrong number of arguments")
+        if not is_symbol(ast[1]):
+            raise DiyLangError("not a symbol")
+        name = ast[1]
+        value = evaluate(ast[2], env)
+        env.set(name, value)
+        return value
+
+    raise DiyLangError(str(str(ast)))
 
 
 def evalParts(ast, env):
